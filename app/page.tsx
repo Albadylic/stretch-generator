5880224fcb7e513ec2d30b4f77694cfc3a74e171
Gregor <43413852+Albadylic@users.noreply.github.com>
2024-10-30 17:26:16 +0000

    Quick setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,90 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+import { generate } from "./api/chat/action";
+import { readStreamableValue } from "ai/rsc";
+
+export const runtime = "edge";
+export const preferredRegion = "home";
+
+export default function App() {
+  const [generation, setGeneration] = useState<string>("");
+
+  const areas = [
+    { value: "back" },
+    { value: "neck" },
+    { value: "arms" },
+    { value: "legs" },
+  ];
+
+  const [state, setState] = useState({
+    area: "",
+    time: "",
+  });
+
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main>
+      <div>
+        <h2>Daily stretch</h2>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      <div>
+        <h3>What would you like to stretch?</h3>
+        {areas.map(({ value }) => (
+          <div key={value}>
+            <input
+              id={value}
+              type="radio"
+              value={value}
+              name="area"
+              onChange={handleChange}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <label htmlFor={value}>{`${value}`}</label>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <h3>How long would you like to stretch?</h3>
+        <div>
+          <input
+            type="range"
+            id="time"
+            name="time"
+            min="10"
+            max="60"
+            step="5"
+          />
+          <label htmlFor="time">Time (mins)</label>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+
+      <button
+        onClick={async () => {
+          const { output } = await generate(
+            `Generate a stretching plan focused on the ${state.area} that will take around ${state.time} minutes`
+          );
+          setGeneration(() => "");
+          for await (const delta of readStreamableValue(output)) {
+            setGeneration(
+              (currentGeneration) => `${currentGeneration}${delta}`
+            );
+          }
+        }}
+      >
+        Generate Plan
+      </button>
+
+      <div>{generation}</div>
+    </main>
   );
 }
